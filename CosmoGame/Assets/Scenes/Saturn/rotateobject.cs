443282ed3307fc511ac3a4c5c91b2f79bd990712
arina-glushchenko <a.glushchenko2@g.nsu.ru>
2024-06-12 04:45:11 +0700

    connected scenes

---
+++ CosmoGame/Assets/Scenes/Saturn/rotateobject.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class create : MonoBehaviour
+{
+    private float timer; // Время движения объекта
+
+    /**
+     * Инициализация переменных класса
+     **/
+    void Start()
+    {
+        timer = 7f;
+    }
+
+    /**
+     * Обновление кадра
+     **/
+    void Update()
+    {
+        timer -= Time.deltaTime; // Изменение времени
+        if (timer <= 0) // Время движения объекта закончилось
+            Destroy(gameObject); // Уничтожение объекта
+        transform.position -= new Vector3(0, 0, 3f); // Изменение позиции объекта
+    }
+}
