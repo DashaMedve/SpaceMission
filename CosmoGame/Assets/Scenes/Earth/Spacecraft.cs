f5c9a983407be376d72ad2bc59f3eff2dced6530
d.medvedeva <d.medvedeva@g.nsu.ru>
2024-06-12 03:42:53 +0700

    Neptune

--- CosmoGame/Assets/Scenes/Earth/Spacecraft.cs
+++ CosmoGame/Assets/Scenes/Earth/Spacecraft.cs
@@ -2,15 +2,10 @@ using UnityEngine;
 
 public class Spacecraft : MonoBehaviour {
     private ItemDisplayManager itemDisplayManager;
-    private Renderer renderer;
-    private Color original_color;
-    public Color new_color;
 
     // Start is called before the first frame update
     void Start() {
         itemDisplayManager = FindObjectOfType<ItemDisplayManager>();
-        renderer = GetComponent<Renderer>();
-        original_color = renderer.material.color;
     }
 
     // Update is called once per frame
@@ -18,13 +13,5 @@ public class Spacecraft : MonoBehaviour {
 
     void OnMouseDown() {
         Debug.Log("Fly to the Space");
-    }
-
-    void OnMouseEnter() {
-        renderer.material.color = new_color;
-    }
-
-    void OnMouseExit() {
-        renderer.material.color = original_color;
     }
 }
